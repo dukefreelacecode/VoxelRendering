c81f61dd154e6172cbd5411f9cb72fc289106489
janismac <janismac@gmx.de>
2013-10-20 23:29:20 +0200

    OpenCL->OpenGL Rendering Pipeline working, YES! :)

--- OpenCLAcceleratedVoxelRendering/AcceleratedVoxelRendering/kernel.cl
+++ OpenCLAcceleratedVoxelRendering/AcceleratedVoxelRendering/kernel.cl
@@ -1,5 +1,14 @@
-__kernel void blub (global char* data, const int num)
+__kernel void blub(__write_only image2d_t outputImage,  const int frames)
 {
-   for (int i = 1; i < 200; i++)
-      data[num+i] = 255;
+	for(int i = 0; i < 256;i++)
+	for(int j = 0; j < 256;j++)
+	{
+		int2 coords = (int2)(i,j);
+		//int4 pixel = (int4)((i<<20)+(j<<12),0,0,255);
+
+		float nanana = (1+sin(.1f*frames))*.5f;
+
+		float4 pixel = (float4)(nanana,j/256.0f,i/256.0f*j/256.0f,1);
+		write_imagef(outputImage, coords, pixel);
+	}
 }
--- OpenCLAcceleratedVoxelRendering/AcceleratedVoxelRendering/main.cpp
+++ OpenCLAcceleratedVoxelRendering/AcceleratedVoxelRendering/main.cpp
@@ -29,6 +29,15 @@ int windowID;
 GLuint textureID;
 
 
+cl_int cl_err;
+cl_platform_id my_cl_platform;
+cl_device_id my_cl_device;
+cl_context my_cl_context;
+cl_command_queue my_cl_command_queue;
+cl_program my_cl_program;
+cl_mem my_cl_buffer;
+cl_kernel my_cl_kernel;
+
 void cl_perr(char* context, cl_int err)
 {
 	const char* msg = clErrorString(err);
@@ -37,6 +46,18 @@ void cl_perr(char* context, cl_int err)
 
 void display()
 {
+	
+	cl_err = clSetKernelArg(my_cl_kernel, 0, sizeof(cl_mem), &my_cl_buffer);
+	cl_err |= clSetKernelArg(my_cl_kernel, 1, sizeof(int), &frameCount);
+	cl_perr("clSetKernelArg", cl_err);
+
+	size_t global_work_size = 1;
+	size_t local_work_size = 1;
+
+	cl_err = clEnqueueNDRangeKernel (my_cl_command_queue, my_cl_kernel, 1, NULL, &global_work_size,  &local_work_size, 0, NULL, NULL);
+	cl_perr("clEnqueueNDRangeKernel", cl_err);
+	clFinish(my_cl_command_queue);
+
     glBegin(GL_QUADS);
     glTexCoord2f(0, 1);
     glVertex3f(-1, 1, 0.0f);
@@ -87,75 +108,60 @@ int main(int argc, char** argv)
 
 
 	glEnable(GL_TEXTURE_2D);
-	glGenTextures(1, &textureID); 
+	glGenTextures(1, &textureID); 	
 	glBindTexture(GL_TEXTURE_2D, textureID);
 
-	const int textureSize = 512;
+	const int textureSize = 256;
 
-	char data[textureSize*textureSize*3];
+	char data[textureSize*textureSize*4];
 	for (int x = 0; x < textureSize; x++)
 	{
 		for (int y = 0; y < textureSize; y++)
 		{
-			for (int c = 0; c < 3; c++)
+			for (int c = 0; c < 4; c++)
 			{
-				data[(x*textureSize+y)*3+c] = (x+y+50*c)%256;
+				data[(x*textureSize+y)*4+c] = (x+y+50*c)%256;
 			}
 		}
 	}
 
 	glTexParameterf(GL_TEXTURE_2D,  GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameterf(GL_TEXTURE_2D,  GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, textureSize, textureSize, 0, GL_RGB,  GL_UNSIGNED_BYTE, data);
+	glTexImage2D(GL_TEXTURE_2D, 0,GL_RGBA, textureSize, textureSize, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 	
-	cl_int cl_err;
-	cl_platform_id cl_platform;
-	cl_device_id cl_device;
-	cl_context cl_context;
-	cl_command_queue cl_command_queue;
-	cl_program cl_program;
-	cl_mem cl_buffer;
-	cl_kernel cl_kernel;
+	
 
-	
-	cl_err = clGetPlatformIDs(1, &cl_platform, NULL);
+	// ==== OpenCL Platform + Device ====
+
+	cl_err = clGetPlatformIDs(1, &my_cl_platform, NULL);
 	cl_perr("clGetPlatformIDs", cl_err);
 
 
-	cl_err = clGetDeviceIDs(cl_platform, CL_DEVICE_TYPE_GPU, 1, &cl_device,	NULL);
-	
-	cl_context_properties properties[] = { 
-	CL_GL_CONTEXT_KHR, (cl_context_properties)wglGetCurrentContext(), // WGL Context 
-	CL_WGL_HDC_KHR, (cl_context_properties)wglGetCurrentDC(),  // WGL HDC
-	CL_CONTEXT_PLATFORM, (cl_context_properties)cl_platform,  // OpenCL platform
-	0};
-
-	cl_device_id devices[32];
-	size_t sharing_devices_count;
-
-
-	//void* func_ptr = clGetExtensionFunctionAddress("clGetGLContextInfoKHR");
-	clGetGLContextInfoKHR_fn *functionPtr = (clGetGLContextInfoKHR_fn*)clGetExtensionFunctionAddress("clGetGLContextInfoKHR");
-	
-	
-	cl_err = (*functionPtr)(properties, CL_CURRENT_DEVICE_FOR_GL_CONTEXT_KHR,32*sizeof(cl_device_id), devices, &sharing_devices_count);
-	cl_perr("clGetDeviceIDs", cl_err);
-
+	cl_err = clGetDeviceIDs(my_cl_platform, CL_DEVICE_TYPE_GPU, 1, &my_cl_device,	NULL);
 
 	char message[20000];
-	cl_err =  clGetDeviceInfo(cl_device, CL_DEVICE_EXTENSIONS, 20000, message, NULL);	
+	cl_err =  clGetDeviceInfo(my_cl_device, CL_DEVICE_EXTENSIONS, 20000, message, NULL);	
 	cl_perr("clGetDeviceInfo", cl_err);
 	cout << "Supported Extensions: " << message << endl;
 
 	char devName[255];
-	cl_err = clGetDeviceInfo(cl_device, CL_DEVICE_NAME, 254, devName, NULL);
+	cl_err = clGetDeviceInfo(my_cl_device, CL_DEVICE_NAME, 254, devName, NULL);
 	cl_perr("clGetDeviceInfo", cl_err);
 	cout << "Device Name: " << devName << endl;
 
 
-	cl_context = clCreateContext (NULL, 1, &cl_device, NULL, NULL, &cl_err);
+	// ===== OpenCL Context =========
+
+	cl_context_properties properties[] = 
+	{ 
+		CL_GL_CONTEXT_KHR, (cl_context_properties)wglGetCurrentContext(), // WGL Context 
+		CL_WGL_HDC_KHR, (cl_context_properties)wglGetCurrentDC(),  // WGL HDC
+		CL_CONTEXT_PLATFORM, (cl_context_properties)my_cl_platform,  // OpenCL platform
+	0};
+
+	my_cl_context = clCreateContext (properties, 1, &my_cl_device, NULL, NULL, &cl_err);
 	cl_perr("clCreateContext", cl_err);
-	cl_command_queue = clCreateCommandQueue (cl_context, cl_device, 0, &cl_err);
+	my_cl_command_queue = clCreateCommandQueue (my_cl_context, my_cl_device, 0, &cl_err);
 	cl_perr("clCreateCommandQueue", cl_err);
 
 
@@ -169,20 +175,20 @@ int main(int argc, char** argv)
 	size_t kernel_file_c_str_size = strlen(kernel_file_c_str);
 
 		
-	cl_program = clCreateProgramWithSource(cl_context, 1, (const char**)&kernel_file_c_str, &kernel_file_c_str_size, &cl_err);
+	my_cl_program = clCreateProgramWithSource(my_cl_context, 1, (const char**)&kernel_file_c_str, &kernel_file_c_str_size, &cl_err);
 	cl_perr("clCreateProgramWithSource", cl_err);
 
-	cl_err = clBuildProgram(cl_program, 1, &cl_device, NULL, NULL, NULL);
+	cl_err = clBuildProgram(my_cl_program, 1, &my_cl_device, NULL, NULL, NULL);
 	cl_perr("clBuildProgram", cl_err);
 
 	if(cl_err != CL_SUCCESS)
 	{
 		char message_buffer[1024*16];
-		clGetProgramBuildInfo (cl_program, cl_device, CL_PROGRAM_BUILD_LOG, 1024*16-1, message_buffer, NULL);
+		clGetProgramBuildInfo (my_cl_program, my_cl_device, CL_PROGRAM_BUILD_LOG, 1024*16-1, message_buffer, NULL);
 		cout << message_buffer << endl;
 	}
 	
-	cl_kernel = clCreateKernel (cl_program, "blub", &cl_err);
+	my_cl_kernel = clCreateKernel (my_cl_program, "blub", &cl_err);
 	cl_perr("clCreateKernel", cl_err);
 
 
@@ -190,22 +196,10 @@ int main(int argc, char** argv)
 	float* cl_buffer_data = new float[size];
 	for (int i = 0; i < size; i++) cl_buffer_data[i]=i;
 
-	//cl_buffer = clCreateBuffer(cl_context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, size*sizeof(float), cl_buffer_data, &cl_err);
+	my_cl_buffer = clCreateFromGLTexture2D(my_cl_context, CL_MEM_WRITE_ONLY, GL_TEXTURE_2D, 0, textureID, &cl_err);
 
-	//cl_buffer = clCreateFromGLTexture2D(cl_context, CL_MEM_READ_WRITE, GL_TEXTURE_2D, 0, textureID, &cl_err);
-	cl_buffer=  clCreateFromGLBuffer  (cl_context, CL_MEM_READ_WRITE, textureID, &cl_err);
 	cl_perr("clCreateBuffer", cl_err);
 
-	int num = 300;
-	cl_err = clSetKernelArg(cl_kernel, 0, sizeof(cl_mem), &cl_buffer);
-	cl_err |= clSetKernelArg(cl_kernel, 1, sizeof(size_t), &num);
-	cl_perr("clSetKernelArg", cl_err);
-
-	size_t global_work_size = 1;
-	size_t local_work_size = 1;
-
-	cl_err = clEnqueueNDRangeKernel (cl_command_queue, cl_kernel, 1, NULL, &global_work_size,  &local_work_size, 0, NULL, NULL);
-	cl_perr("clEnqueueNDRangeKernel", cl_err);
 
 	/*cl_err = clEnqueueReadBuffer(cl_command_queue , cl_buffer, CL_TRUE, 0, size*sizeof(float), cl_buffer_data, 0, NULL, NULL);
 	cl_perr("clEnqueueReadBuffer", cl_err);
